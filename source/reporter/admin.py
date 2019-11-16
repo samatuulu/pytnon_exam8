6bc2e6859ded2e94778eb195c0c2c699a865c820
Bektursun <samatuulu11@gmail.com>
2019-11-16 13:04:20 +0600

    Created models Product and Review. Tested with some fake data. Created dump.json. Done first step.

--- source/reporter/admin.py
+++ source/reporter/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from reporter.models import Product, Review
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'description', 'photo']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name', 'description']
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'author', 'product', 'feedback', 'point']
+    list_filter = ['author', 'product']
+    list_display_links = ['pk', 'author']
+    search_fields = ['author', 'feecback']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
--- source/reporter/models.py
+++ source/reporter/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('other', 'Other'),
+    ('food', 'Food'),
+    ('clothes', 'Clothes'),
+    ('household', 'Home items'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Product')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Category')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Photo')
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, related_name='reporter', on_delete=models.CASCADE, verbose_name='Author')
+    product = models.ForeignKey(Product, related_name='product', on_delete=models.CASCADE, verbose_name='Product')
+    feedback = models.TextField(max_length=2000, verbose_name='Feedback')
+    point = models.IntegerField(null=False, blank=False, verbose_name='Point')
+
+    def __str__(self):
+        return str(self.author)
